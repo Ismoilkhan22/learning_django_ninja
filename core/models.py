5771afcaf85d93bcff61af62b873add0c7b96b7c
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-04-28 21:52:18 +0500

    models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
+from django.db import models
 
 
+class Tract(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=250)
+    duration = models.FloatField()
+    last_display = models.DateTimeField()
 
